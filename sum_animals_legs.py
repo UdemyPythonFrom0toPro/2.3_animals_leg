62df1ef7aaa692dda4439739fc1b0edbb8033ecb
Firuz Amirov <59014178+firuzamiroff@users.noreply.github.com>
2020-12-21 03:19:37 +0400

    Create sum_animals_legs.py

---
+++ sum_animals_legs.py
@@ -0,0 +1,5 @@
+chickens = int(input("How many chicken\n> "))
+pigs = int(input("How many pigs\n> "))
+cows = int(input("How many cows\n> "))
+total_legs = chickens * 2 + pigs * 4 + cows * 4
+print(total_legs)
